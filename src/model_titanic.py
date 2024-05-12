9392095b3d476d4c777e3ceb4a644088b4d74cf2
lastinm <lastinm@mail.ru>
2024-05-12 17:04:08 +0300

    Improved model

--- src/model_titanic.py
+++ src/model_titanic.py
@@ -1,6 +1,8 @@
+# import pandas as pd
 from catboost.datasets import titanic
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, classification_report
 import os
 
@@ -25,15 +27,26 @@ try:
 except Exception as e:
     print("An error occurred while saving datasets:", e)
 
-# Разделение на признаки и целевую переменную
-X = train_df.drop('Survived', axis=1)
+# Заполнение пропущенных значений
+train_df['Age'].fillna(train_df['Age'].median(), inplace=True)
+train_df['Embarked'].fillna(train_df['Embarked'].mode()[0], inplace=True)
+
+# Преобразование категориальных признаков
+label_encoders = {}
+for col in ['Sex', 'Embarked']:
+    le = LabelEncoder()
+    train_df[col] = le.fit_transform(train_df[col])
+    label_encoders[col] = le
+
+# Разделение данных на признаки и целевую переменную
+X = train_df.drop(['Survived', 'Name', 'Ticket', 'Cabin'], axis=1)
 y = train_df['Survived']
 
 # Разделение на обучающий и тестовый наборы
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Обучение модели случайного леса
-rf_model = RandomForestClassifier()
+rf_model = RandomForestClassifier(random_state=42)
 rf_model.fit(X_train, y_train)
 
 # Предсказание на тестовом наборе
@@ -41,7 +54,7 @@ y_pred = rf_model.predict(X_test)
 
 # Оценка качества модели
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}')
+print(f"Accuracy: {accuracy}")
 
 print('Classification Report:')
 print(classification_report(y_test, y_pred)[:10])
