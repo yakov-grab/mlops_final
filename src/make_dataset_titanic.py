a57d105365dd7a69749951015ce6f04c1b0da77c
lastinm <lastinm@mail.ru>
2024-05-12 16:18:42 +0300

    Refactoring filenames

--- src/make_dataset_titanic.py
+++ src/make_dataset_titanic.py
@@ -2,26 +2,26 @@ import pandas as pd
 from catboost.datasets import titanic
 
 train_df, test_df = titanic()
-print("Информация об исходном датасете:")
+print("Information about the native dataset:")
 print(train_df.info())
 
 # Заполнение пропущенных значений в поле "Age" средним значением
 mean_age = train_df['Age'].mean()
-print(f"Средний возраст: {mean_age}")
+print(f"Average age: {mean_age}")
 
 train_df['Age'] = train_df['Age'].fillna(mean_age)
 test_df['Age'] = test_df['Age'].fillna(mean_age)
 
-print("Информация о датасете после замены пропущенных значений:")
+print("Information about the dataset after replacing missing results:")
 print(train_df.info())
 
 try:
     # Сохранение обновленных датасетов в CSV
     train_df.to_csv('../datasets/train.csv', index=False)
     test_df.to_csv('../datasets/test.csv', index=False)
-    print("Датасеты успешно сохранены в каталоге ../dataset под именами train.csv и test.csv.")
+    print("Datasets successfully saved in the directory ../dataset under names train.csv and test.csv.")
 except Exception as e:
-    print("Произошла ошибка при сохранении датасетов:", e)
+    print("An error occurred while saving datasets:", e)
 
 # print("Train dataset:")
 # print(train_df.head())
